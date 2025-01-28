c7074788a7f471ebe04486914b5a063ace071e74
Viliam Tencer <vilten@vilten.sk>
2025-01-28 18:42:39 +0100

    first app

---
+++ Avalonia Monogame Dock Template/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Avalonia_Monogame_Dock_Template
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
